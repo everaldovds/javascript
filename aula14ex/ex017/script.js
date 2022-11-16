e524b7eb63043c1f56f9133227ae4c4ee16bcbc6
Everaldo Silva <everaldovarela.contato@gmail.com>
2022-11-16 20:29:19 -0300

    Update aula 14

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,1 +1,9 @@
-alert('ola!')
+function tabuada() {
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+    if (num.value.length == 0) {
+        window.alert('Por favor, digite um número!')
+    } else {
+        let n = Number(num.value)
+    } 
+}
